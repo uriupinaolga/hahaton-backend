92ed8e4db1707ae1c6a532624ab703516a7dd4c9
uriupinaolga <uriupina.olga@icloud.com>
2020-10-25 12:21:04 +0300

    fix

--- src/palette.js
+++ src/palette.js
@@ -1,21 +1,40 @@
 import ColorThief from 'colorthief'
 import to from 'await-to-js'
 import ml from './ml'
+import namer from 'color-namer'
+
+const rgbToHex = (r, g, b) => {
+    return '#'+((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
 
 export default async () => {
     const img = './src/image.jpg';
 
-    const [errorDominant, dominant] = await to(ColorThief.getColor(img))
+    const [errorDominant, dominantRgb] = await to(ColorThief.getColor(img))
     if (errorDominant) throw errorDominant;
 
-    const [errorPalette, palette] = await to(ColorThief.getPalette(img, 15))
+    const [errorPalette, palettesRgb] = await to(ColorThief.getPalette(img, 15))
     if (errorPalette) throw errorPalette
+
+    const hexDominant = rgbToHex(...dominantRgb).toString().toUpperCase();
+
+    const dominant = {
+        color: hexDominant,
+        name: namer(hexDominant).ntc[0].name,
+    }
+
+    const palette = palettesRgb.map(palette => {
+        const hexPalette = rgbToHex(...palette).toString().toUpperCase();
+        return {
+            color: hexPalette,
+            name: namer(hexPalette).ntc[0].name,
+        }
+    });
 
     const result = {dominant, palette}
 
     try {
-        const mlDominant = ml.dominant(dominant)
-        // const mlPalette = ml.palette(palette)
+        const mlDominant = ml.dominant(dominantRgb)
 
         return {...result, ...mlDominant}
     } catch (e) {
